ecc0a1a049ed594946fafe36da8a4d0e618c469b
JacksonPassos <jacksonguitar33@gmail.com>
2020-07-25 20:36:56 -0300

    cadastrando, validando data e criando repositórios

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -3,9 +3,9 @@ const app = express()
 
 import routes from './routes'
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack!' })
-})
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3333, ()=> {
   console.log('🚀 Server started on port 3333!')
